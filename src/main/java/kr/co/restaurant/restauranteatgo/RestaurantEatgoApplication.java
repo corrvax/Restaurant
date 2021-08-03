09689092a7a493ea10ef8588066b2afb3d0f8c6e
MJ-coding <zmffheldk@gmail.com>
2021-08-03 16:00:51 +0900

    git init

---
+++ src/main/java/kr/co/restaurant/restauranteatgo/RestaurantEatgoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.restaurant.restauranteatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantEatgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantEatgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/restaurant/restauranteatgo/RestaurantEatgoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.restaurant.restauranteatgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantEatgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
